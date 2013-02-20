f5c7270235414a402c55890434a466f04a6edc2c
Carlos Villela <cv@lixo.org>
2013-02-20 15:29:58 -0300

    install and configure postgresql (using puppetlabs-postgresql module… maybe there is a better place to put it?)

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,3 +1,26 @@
+stage {
+  'bootstrap':
+    before => Stage['main']
+  ;
+}
+
 node default {
+
+  class { 'apt':
+    stage             => 'bootstrap',
+    always_apt_update => true,
+  }
+
+  include postgresql::server
+  include postgresql::client
+  include postgresql::devel
+
+  postgresql::db {
+    'vcv':
+      user     => 'vcv',
+      password => 'vcv',
+      charset  => 'UTF-8',
+    ;
+  }
 
 }
