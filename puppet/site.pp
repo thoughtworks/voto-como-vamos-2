cc0c3e6361d1e5c922a7ad80aaa1b0e3db9a9368
Carlos Villela <cv@lixo.org>
2013-02-24 02:54:21 -0300

    add hstore extension to pgsql, trying to fix bundler/passenger dependency chain

--- puppet/site.pp
+++ puppet/site.pp
@@ -4,6 +4,23 @@ stage {
   ;
 }
 
+define postgresql::extension($extension, $db = $title) {
+
+  include postgresql::params
+
+  postgresql_psql {
+    "add extension ${extension} to ${db}":
+      command   => "CREATE EXTENSION ${extension}",
+      unless    => "SELECT extname FROM pg_extension WHERE extname='${extension}'",
+      require   => Postgresql::Db[$db],
+      psql_user => 'postgres',
+      db        => $db,
+      cwd       => $postgresql::params::datadir,
+    ;
+  }
+
+}
+
 node default {
 
   class {
@@ -18,27 +35,49 @@ node default {
   include postgresql::client
   include postgresql::devel
 
-  postgresql::db {
+  package {
+    'postgresql-contrib':
+      ensure  => 'installed',
+    ;
+  }
 
-    'vcv_production':
-      user     => 'vcv',
-      password => 'vcv',
-      charset  => 'UTF-8',
+  postgresql::extension {
+
+    'vcv_development':
+      extension => 'hstore',
     ;
 
     'vcv_test':
-      user     => 'vcv',
-      password => 'vcv',
-      charset  => 'UTF-8',
+      extension => 'hstore',
+    ;
+
+    'vcv_production':
+      extension => 'hstore',
+    ;
+
+  }
+
+  postgresql::db {
+
+    'vcv_production':
+      user      => 'vcv',
+      password  => 'vcv',
+      charset   => 'UTF-8',
+    ;
+
+    'vcv_test':
+      user      => 'vcv',
+      password  => 'vcv',
+      charset   => 'UTF-8',
     ;
 
     'vcv_development':
-      user     => 'vcv',
-      password => 'vcv',
-      charset  => 'UTF-8',
+      user      => 'vcv',
+      password  => 'vcv',
+      charset   => 'UTF-8',
     ;
 
- }
+  } -> Package['postgresql-contrib']
 
   rbenv::install {
     'vagrant':
@@ -60,11 +99,9 @@ node default {
 
   class {
     'apache':
-
     ;
 
     'passenger':
-      require                => Rbenv::Compile['1.9.3-p385'],
       passenger_package      => 'passenger',
       passenger_version      => $passenger_version,
       passenger_ruby         => '/home/vagrant/.rbenv/shims/ruby',
@@ -72,7 +109,7 @@ node default {
       gem_binary_path        => $gem_bin_path,
       mod_passenger_location => "${gem_path}/passenger-${passenger_version}/ext/apache2/mod_passenger.so",
     ;
-  }
+  } -> Bundler::Install['/vagrant']
 
   apache::vhost {
     'vcv':
@@ -89,6 +126,7 @@ node default {
   bundler::install {
     '/vagrant':
       gem_bin_path => $gem_bin_path,
+      require      => Rbenv::Compile['1.9.3-p385'],
     ;
   }
 }
