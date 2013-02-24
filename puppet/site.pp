1c28e0ddebd72674a22036f98cd1d549d6051ad5
Carlos Villela <cv@lixo.org>
2013-02-24 13:59:26 -0300

    remove (unnecessary?) stage

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,18 +1,6 @@
-stage {
-  'bootstrap':
-    before => Stage['main']
-  ;
-}
-
 node default {
-
-  class {
-    'apt':
-      stage             => 'bootstrap',
-      always_apt_update => false,
-    ;
-  }
 
   include votocomovamos::db
   include votocomovamos::app
+
 }
