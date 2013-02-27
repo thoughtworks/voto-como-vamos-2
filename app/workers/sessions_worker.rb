34cebfdd446f216334b2fb9f01dc1b4e39d8bd46
Carlos Villela <cv@lixo.org>
2013-02-27 13:54:33 -0300

    inline method, more clean ups

--- app/workers/sessions_worker.rb
+++ app/workers/sessions_worker.rb
@@ -27,54 +27,44 @@ class SessionVotesPageWorker
       a
     end
 
-    data = votacoes.map {|votacao| detalhes_da votacao, session_id }
-    ScraperWiki::save_sqlite(%w[sessao_uuid horario_inicio], data, 'votacoes')
-  end
+    data = votacoes.map do |votacao|
+      votacao_detalhes = begin
+        Nokogiri::HTML open(URI.join(BASE_URL, votacao[:detalhes_link]).to_s).read.force_encoding('UTF-8')
+      rescue => e
+        puts "Got exception #{e}"
+        retry
+      end
 
-  def detalhes_da(votacao, sessao)
-    votacao_detalhes = begin
-      Nokogiri::HTML open(URI.join(BASE_URL, votacao[:detalhes_link]).to_s).read.force_encoding('UTF-8')
-    rescue => e
-      puts "Got exception #{e}"
-      retry
-    end
+      text = votacao_detalhes.xpath('//div[@class="box no-box"]').text
 
-    text = votacao_detalhes.xpath('//div[@class="box no-box"]').text
-
-    {
-      uuid: sessao[:uuid],
-      data: sessao[:data],
-      horario_inicio: votacao[:horario],
-      horario_encerramento: text.match(/Encerramento:\s+((\d{2}:?){3})/)[1],
-      proposicao: votacao[:proposicao],
-      tipo: votacao[:tipo],
-      situacao: votacao[:situacao],
-      item_pauta: text.match(/Item pauta:\s+([^\n]+)\n/)[1],
-      ordem_dia: text.match(/Ordem dia:\s+([^\n]+) Resultado/)[1],
-    }.tap do |votacao|
-      data = presencas_da(votacao_detalhes, votacao, sessao)
-      ScraperWiki::save_sqlite(%w[data_sessao tipo_sessao numero_sessao horario_inicio_votacao parlamentar], data, 'presencas')
-    end
-  end
-
-  def presencas_da(votacao_detalhes, votacao, sessao)
-    table = votacao_detalhes.css('table.list tr')
-    table.shift # discarta header
-    table.map {|tr| tr.text.split(/\n/)[0,3].map &:strip }.inject([]) do |a, e|
-      a << {
-        data_sessao: sessao[:data],
-        tipo_sessao: sessao[:tipo],
-        numero_sessao: sessao[:numero],
-
-        horario_inicio_votacao: votacao[:horario_inicio],
-        parlamentar: e[0],
-        partido: e[1],
-        voto: e[2],
+      ballot = AssemblyBallot.save! {
+        uuid: sessao[:uuid],
+        data: sessao[:data],
+        horario_inicio: votacao[:horario],
+        horario_encerramento: text.match(/Encerramento:\s+((\d{2}:?){3})/)[1],
+        proposicao: votacao[:proposicao],
+        tipo: votacao[:tipo],
+        situacao: votacao[:situacao],
+        item_pauta: text.match(/Item pauta:\s+([^\n]+)\n/)[1],
+        ordem_dia: text.match(/Ordem dia:\s+([^\n]+) Resultado/)[1],
       }
+
+      table = votacao_detalhes.css('table.list tr')
+      table.shift # discarta header
+      table.map {|tr| tr.text.split(/\n/)[0,3].map &:strip }.each do |e|
+        AssemblyVote.save! {
+          data_sessao: sessao[:data],
+          tipo_sessao: sessao[:tipo],
+          numero_sessao: sessao[:numero],
+
+          horario_inicio_votacao: votacao[:horario_inicio],
+          parlamentar: e[0],
+          partido: e[1],
+          voto: e[2],
+        }
+      end
     end
   end
-
-
 
 end
 
