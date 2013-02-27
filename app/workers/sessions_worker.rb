7a66cdecb7f9fce10dd0ae55e0efbb1a42627334
Carlos Villela <cv@lixo.org>
2013-02-27 13:38:35 -0300

    more splitting stuff out and de-scraperwiki-fication

--- app/workers/sessions_worker.rb
+++ app/workers/sessions_worker.rb
@@ -3,16 +3,32 @@ require 'uri'
 require 'nokogiri'
 require 'digest/md5'
 
-class SessionListPageWorker
+class SessionVotesPageWorker
 
   include Sidekiq::Worker
 
-  def perform(index)
-    links = index.css('a.sessoes @href').map &:text
+  BASE_URL = 'http://votacoes.camarapoa.rs.gov.br/'
 
-    links.map do |link|
-      processa_sessao(link)
+  def perform(session_id, session_html)
+    tabela = session_html.css('table tr').map(&:text).reject {|x| x == "" }.map {|x| x.split(/\t\t/).map &:strip }
+    detalhes_links = session_html.css('table tr a @href').map(&:text).uniq
+    _ = tabela.shift # discarta headers
+
+    i = 0
+    votacoes = tabela.inject([]) do |a, e|
+      a << {
+        horario: e[0],
+        proposicao: e[1],
+        tipo: e[2],
+        situacao: e[4],
+        detalhes_link: detalhes_links[i].gsub(/parlamentares\?/, 'parlamentares_nome?')
+      }
+      i += 1
+      a
     end
+
+    data = votacoes.map {|votacao| detalhes_da votacao, session_id }
+    ScraperWiki::save_sqlite(%w[sessao_uuid horario_inicio], data, 'votacoes')
   end
 
   def presencas_da(votacao_detalhes, votacao, sessao)
@@ -58,49 +74,33 @@ class SessionListPageWorker
     end
   end
 
-  def votacoes_da(sessao, sessao_hash)
-    tabela = sessao.css('table tr').map(&:text).reject {|x| x == "" }.map {|x| x.split(/\t\t/).map &:strip }
-    detalhes_links = sessao.css('table tr a @href').map(&:text).uniq
-    _ = tabela.shift # discarta headers
 
-    i = 0
-    votacoes = tabela.inject([]) do |a, e|
-      a << {
-        horario: e[0],
-        proposicao: e[1],
-        tipo: e[2],
-        situacao: e[4],
-        detalhes_link: detalhes_links[i].gsub(/parlamentares\?/, 'parlamentares_nome?')
-      }
-      i += 1
-      a
-    end
+end
 
-    Thread.new do
-      data = votacoes.map {|votacao| detalhes_da votacao, sessao_hash }
-      ScraperWiki::save_sqlite(%w[sessao_uuid horario_inicio], data, 'votacoes')
-    end
-  end
+class SessionPageWorker
 
-  def processa_sessao(link)
-    sessao = begin
-      Nokogiri::HTML open(URI.join(BASE_URL, link).to_s).read.force_encoding('UTF-8')
+  include Sidekiq::Worker
+
+  BASE_URL = 'http://votacoes.camarapoa.rs.gov.br/'
+
+  def perform(url)
+    html = begin
+      Nokogiri::HTML open(URI.join(BASE_URL, url).to_s).read
     rescue => e
       puts "Got exception #{e}"
       retry
     end
 
-    titulo = sessao.css('#sessao_mais_recente').text.split(/\n/)[2].strip.split
+    titulo = html.css('#sessao_mais_recente').text.split(/\n/)[2].strip.split
 
-    {
-      sessao_uuid: Digest::MD5.hexdigest("#{titulo[0]}-#{titulo[4]}-#{titulo[2].gsub(/[^\d]/, '')}"),
-      data: titulo[0],
-      tipo: titulo[4],
+    as = AssemblySession.create! {
+      uuid:   Digest::MD5.hexdigest("#{titulo[0]}-#{titulo[4]}-#{titulo[2].gsub(/[^\d]/, '')}"),
+      data:   titulo[0],
+      tipo:   titulo[4],
       numero: titulo[2].gsub(/[^\d]/, ''),
-    }.tap do |result|
-      ScraperWiki::save_sqlite(%w[data tipo numero], result, 'sessoes')
-      votacoes_da(sessao, result)
-    end
+    }
+
+    SessionVotesPageWorker.perform_async as.uuid, html
   end
 
 end
@@ -111,7 +111,10 @@ class SessionsWorker
 
   def perform(url=BASE_URL)
     index = Nokogiri::HTML open(url).read
-    SessionListPageWorker.perform_async index
+
+    index.css('a.sessoes @href').each do |href|
+      SessionPageWorker.perform_async href.text
+    end
 
     next_link = index.css('a.next_page @href').first
     SessionsWorker.perform_async URI.join(BASE_URL, next_link.text).to_s if next_link
