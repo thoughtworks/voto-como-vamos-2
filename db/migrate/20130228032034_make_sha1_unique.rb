581b02569448db0687c123289004a9c1940541cc
Carlos Villela <cv@lixo.org>
2013-02-28 00:22:14 -0300

    make sha1 unique

---
+++ db/migrate/20130228032034_make_sha1_unique.rb
@@ -0,0 +1,7 @@
+class MakeSha1Unique < ActiveRecord::Migration
+
+  def up
+    add_index :scraped_data, :sha1, unique: true
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130227221112) do
+ActiveRecord::Schema.define(:version => 20130228032034) do
 
   create_table "scraped_data", :force => true do |t|
     t.string   "kind",       :null => false
@@ -22,5 +22,6 @@ ActiveRecord::Schema.define(:version => 20130227221112) do
   end
 
   add_index "scraped_data", ["data"], :name => "index_scraped_data_on_data"
+  add_index "scraped_data", ["sha1"], :name => "index_scraped_data_on_sha1", :unique => true
 
 end
