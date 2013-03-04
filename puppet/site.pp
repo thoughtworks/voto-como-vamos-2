d95b013e1833f1a8fca033cf1d839ec141f4f1b5
Carlos Villela <cv@lixo.org>
2013-03-03 23:12:46 -0300

    whitespace

--- puppet/site.pp
+++ puppet/site.pp
@@ -2,11 +2,13 @@ node default {
 
   $max_update_threshold = 640800
 
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update',
-    onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= ${max_update_threshold} ))'",
+  exec {
+    'apt-get update':
+      command => '/usr/bin/apt-get update',
+      onlyif  => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= ${max_update_threshold} ))'",
     ;
   }
+
   Exec['apt-get update'] -> Package <| |>
 
   class{ 'votocomovamos::db': } -> class { 'votocomovamos::app': }
