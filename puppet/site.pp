4d44885f9f7fc79ed6f95dcb5448e0bef64342b5
Rafael Lisboa <rafaelzlisboa@gmail.com>
2013-04-23 20:40:15 -0300

    using apt class to run apt-get update before every package

--- puppet/site.pp
+++ puppet/site.pp
@@ -2,14 +2,9 @@ node default {
 
   $max_update_threshold = 640800
 
-  exec {
-    'apt-get update':
-      command => '/usr/bin/apt-get update',
-      onlyif  => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= ${max_update_threshold} ))'",
-    ;
-  }
-
-  Exec['apt-get update'] -> Package <| |>
+  class { 'apt':
+    always_apt_update => true
+  } -> Package <| |>
 
   class { 'votocomovamos::db': } -> class { 'votocomovamos::app': } -> class { 'votocomovamos::sidekiq': }
 
